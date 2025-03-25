6ea0d5804817918f7dbadb4572efddfbf4f93755
Adarsh Vishwakarma <adarsh.cs12level@gmail.com>
2025-03-25 16:59:38 +0530

    adding HTML file

---
+++ projectCRM/users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request, **kwargs):
+	return render(
+		request, 
+		'users/home.html', 
+		context={}
+	)
+
+def about(request, **kwargs):
+	return HttpResponse("This is an About Page")
