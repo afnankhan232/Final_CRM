688a3a299747860e230b35745b84ae56e097f503
Adarsh Vishwakarma <adarsh.cs12level@gmail.com>
2025-03-26 17:00:27 +0530

    added dummy data

--- projectCRM/users/urls.py
+++ projectCRM/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='user-home'),
+    path('login/', views.login, name='user-home'),
+    path('myCompany/', views.home, name='user-home-page'),
     path('about/', views.about, name='user-about')
 ]
